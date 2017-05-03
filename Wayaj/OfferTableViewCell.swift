ddd446ef685bd149d93e5e91960c4eb046592356
admin <gsingh622@yahoo.com>
2017-05-03 17:14:29 -0400

    v1

---
+++ Wayaj/OfferTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  OfferTableViewCell.swift
+//  Wayaj
+//
+//  Created by Admin on 5/3/17.
+//  Copyright © 2017 GRC. All rights reserved.
+//
+
+import UIKit
+
+class OfferTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
