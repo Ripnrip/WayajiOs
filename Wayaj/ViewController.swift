4eca1649ec99ac856fc79871b99d75918f8c6e8f
Gurinder Singh <gsingh@gedeongrc.com>
2017-05-25 14:30:27 -0400

    Updated tutorial page

--- Wayaj/ViewController.swift
+++ Wayaj/ViewController.swift
@@ -86,14 +86,15 @@ extension ViewController: PaperOnboardingDelegate {
 extension ViewController: PaperOnboardingDataSource {
     
     func onboardingItemAtIndex(_ index: Int) -> OnboardingItemInfo {
-        let titleFont = UIFont(name: "Nunito-Bold", size: 36.0) ?? UIFont.boldSystemFont(ofSize: 36.0)
+        let titleFont = UIFont(name: "Nunito-Bold", size: 34.0) ?? UIFont.boldSystemFont(ofSize: 36.0)
         let descriptionFont = UIFont(name: "OpenSans-Regular", size: 14.0) ?? UIFont.systemFont(ofSize: 14.0)
         
-        let item1 = (imageName: "Hotels", title: "Hotels", description: "All hotels and hostels are sorted by hospitality rating", iconName: "Key", color: UIColor(red:97/255, green:198/255, blue:97/255, alpha:1.00), titleColor: UIColor.white, descriptionColor: UIColor.white, titleFont: titleFont, descriptionFont: descriptionFont)
+        let item1 = (imageName: "intro-icon1", title: "Welcome to Wayaj", description: "The first app for sustainable and socially responsible vacations.", iconName: "Key", color: UIColor(red:97/255, green:198/255, blue:97/255, alpha:1.00), titleColor: UIColor.white, descriptionColor: UIColor.white, titleFont: titleFont, descriptionFont: descriptionFont)
         
-        let item2 = (imageName: "Banks", title: "Banks", description: "We carefully verify all banks before add them into the app", iconName: "Wallet", color: UIColor(red:97/255, green:198/255, blue:97/255, alpha:1.00), titleColor: UIColor.white, descriptionColor: UIColor.white, titleFont: titleFont, descriptionFont: descriptionFont)
+        let item2 = (imageName: "intro-icon4", title: "Discover", description: "The Wayaj app let you explore destinations and book eco-friendly trips", iconName: "Wallet", color: UIColor(red:97/255, green:198/255, blue:97/255, alpha:1.00), titleColor: UIColor.white, descriptionColor: UIColor.white, titleFont: titleFont, descriptionFont: descriptionFont)
+        let item3 = (imageName: "intro-icon3", title: "Book", description: "The Wayaj app let you explore destinations and book eco-friendly trips", iconName: "Wallet", color: UIColor(red:97/255, green:198/255, blue:97/255, alpha:1.00), titleColor: UIColor.white, descriptionColor: UIColor.white, titleFont: titleFont, descriptionFont: descriptionFont)
         
-        let item3 = (imageName: "Stores" , title: "Stores", description: "All local stores are categorized for your convenience", iconName: "Shopping-cart", color: UIColor(red:97/255, green:198/255, blue:97/255, alpha:1.00), titleColor: UIColor.white, descriptionColor: UIColor.white, titleFont: titleFont, descriptionFont: descriptionFont)
+        let item4 = (imageName: "intro-icon5" , title: "Enjoy!🙏", description: "Choose the perfect hotel for you by learning about the sustainability of your destination based on a detailed eco-rating system.", iconName: "Shopping-cart", color: UIColor(red:97/255, green:198/255, blue:97/255, alpha:1.00), titleColor: UIColor.white, descriptionColor: UIColor.white, titleFont: titleFont, descriptionFont: descriptionFont)
         
         switch index {
         case 0:
@@ -104,6 +105,9 @@ extension ViewController: PaperOnboardingDataSource {
             break
         case 2:
             return item3
+            break
+        case 3:
+            return item4
             break
         default:
             return item1
@@ -124,7 +128,7 @@ extension ViewController: PaperOnboardingDataSource {
     }
     
     func onboardingItemsCount() -> Int {
-        return 3
+        return 4
     }
 }
 
