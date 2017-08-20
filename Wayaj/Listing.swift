43903666c093650a67d6c2d93549d4da0b754d40
Admin <admin@Admins-Air-2.home>
2017-08-20 15:21:27 -0400

    updated Realm user, and made explore home cells match up to design doing the second page for listing next

---
+++ Wayaj/Listing.swift
@@ -0,0 +1,9 @@
+//
+//  Listing.swift
+//  Wayaj
+//
+//  Created by Admin on 8/18/17.
+//  Copyright © 2017 GRC. All rights reserved.
+//
+
+import Foundation
---
+++ Wayaj/User.swift
@@ -0,0 +1,9 @@
+//
+//  User.swift
+//  Wayaj
+//
+//  Created by Admin on 8/18/17.
+//  Copyright © 2017 GRC. All rights reserved.
+//
+
+import Foundation
